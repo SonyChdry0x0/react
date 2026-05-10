9c6f5a7bf4e3d80f6c0cc10e53489ff5a9746d01
sony chaudhary <chaudharysony336@gmail.com>
2026-05-10 07:54:09 +0545

    state and handling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Navbar title="TextUtils" aboutText="About Textutils"/>
       {/* <Navbar title = "TextUtils"/> */}
       {/* <Navbar/> */}
-      <div className="container">
+      <div className="container my-3" >
       <TextForm heading = "Enter the text to analyze"/>
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,9 +18,9 @@ export default function TextForm(props) {
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="Mybox" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
-      <button className="btn-btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="form-control" onClick={handleUpClick}>Convert to UpperCase</button>
     </div>
   );
 }
