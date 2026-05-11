e7b1ecd0f14fdc5416618921d28519f993bc4615
sony chaudhary <chaudharysony336@gmail.com>
2026-05-11 08:43:53 +0545

    state and handling

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,15 +1,19 @@
 import React,{useState} from "react";
 
 export default function TextForm(props) {
-    const[text,setText] = useState("Enter text here2");
-    const handleUpClick = ()=>{
-        console.log("uppercase was clicked");
-        setText("you have clicked on handleUpClick")
-    }
-    const handleOnChange =()=>{
-        console.log("Uppercase was clicked");
-    }
-
+  const handleUpClick = ()=>{
+    console.log("uppercase was clicked"+ text);
+    let newText = text.toUpperCase();
+    setText(newText)
+    
+  }
+  const handleOnChange =(event)=>{
+    console.log("On change");
+    setText(event.target.value);
+    
+  }
+  
+  const[text,setText] = useState("Enter text here2");
    // text="new text"; //wrong way to change the state
     //setText("new text"); //correct way to change the state . 
     //
@@ -18,9 +22,9 @@ export default function TextForm(props) {
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="Mybox" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
-      <button className="form-control" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn-btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
     </div>
   );
 }
