72ccd043ffa9fd3141f160194db443ad03ec584b
sony chaudhary <chaudharysony336@gmail.com>
2026-05-22 15:11:42 +0545

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
+// import "./App.css";
+// import About from "./components/About";
+// import Navbar from "./components/Navbar";
+// import TextForm from "./components/TextForm";
+// import React, { useState } from "react";
+// import Alert from "./components/Alert";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// function App() {
+//   const [mode, setMode] = useState("light");
+
+//   const [alert, setAlert] = useState(null);
+//   const showAlert = (message, type) => {
+//     setAlert({
+//       msg: message,
+//       type: type,
+//     });
+//     setTimeout(() => {
+//       setAlert(null);
+//     }, 1500);
+//   };
+//   const [themeColor, setThemeColor] = useState("#0d6efd");
+
+//   const changeColor = (color, name) => {
+//     if (mode === "dark") {
+//       document.body.style.backgroundColor = color;
+//       setThemeColor(color);
+//       showAlert(name + " theme enabled", "success");
+//     }
+//   };
+
+//   const toggleMode = () => {
+//     if (mode === "light") {
+//       setMode("dark");
+//       document.body.style.backgroundColor = "#042743";
+//       showAlert("Dark mode has been enabled", "success");
+//       setInterval(() => {
+//         document.title = "Textutils is amazing mode";
+//       }, 2000);
+//       setInterval(() => {
+//         document.title = "Install Textutils Now";
+//       }, 1500);
+//     } else {
+//       setMode("light");
+//       document.body.style.backgroundColor = "white";
+//       showAlert("light mode has been enabled", "success");
+//     }
+//   };
+//   return (
+//     //className = classNameName
+//     //for =htmlFor
+//     //tabindex = tabIndex
+//     //a= Link 
+//     //href=to
+
+//     <BrowserRouter>
+//       <Navbar
+//         title="TextUtils"
+//         mode={mode}
+//         toggleMode={toggleMode}
+//         changeColor={changeColor}
+//       />
+//       <Alert alert={alert} />
+//       {/* <Navbar title = "TextUtils"/> */}
+//       {/* <Navbar/> */}
+//       <div className="container my-3">
+//         <Routes>
+//           <Route path="/about" element={<About />}></Route>
+//           <Route
+//             path="/"
+//             element={
+//               <TextForm
+//                 showAlert={showAlert}
+//                 heading="Enter the text to analyze"
+//                 mode={mode}
+//                 themeColor={themeColor}
+//               />
+//             }
+//           ></Route>
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
 import "./App.css";
-import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+
 function App() {
   const [mode, setMode] = useState("light");
-
   const [alert, setAlert] = useState(null);
+  const [themeColor, setThemeColor] = useState("#0d6efd");
+
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type,
     });
+
     setTimeout(() => {
       setAlert(null);
     }, 1500);
   };
-  const [themeColor, setThemeColor] = useState("#0d6efd");
 
   const changeColor = (color, name) => {
     if (mode === "dark") {
@@ -33,52 +121,42 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
+
       setInterval(() => {
         document.title = "Textutils is amazing mode";
       }, 2000);
+
       setInterval(() => {
         document.title = "Install Textutils Now";
       }, 1500);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("light mode has been enabled", "success");
+      showAlert("Light mode has been enabled", "success");
     }
   };
-  return (
-    //className = classNameName
-    //for =htmlFor
-    //tabindex = tabIndex
-    //a= Link 
-    //href=to
 
-    <BrowserRouter>
+  return (
+    <>
       <Navbar
         title="TextUtils"
         mode={mode}
         toggleMode={toggleMode}
         changeColor={changeColor}
       />
+
       <Alert alert={alert} />
-      {/* <Navbar title = "TextUtils"/> */}
-      {/* <Navbar/> */}
+
       <div className="container my-3">
-        <Routes>
-          <Route path="/about" element={<About />}></Route>
-          <Route
-            path="/"
-            element={
-              <TextForm
-                showAlert={showAlert}
-                heading="Enter the text to analyze"
-                mode={mode}
-                themeColor={themeColor}
-              />
-            }
-          ></Route>
-        </Routes>
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze"
+          mode={mode}
+          themeColor={themeColor}
+        />
+        <About/>
       </div>
-    </BrowserRouter>
+    </>
   );
 }
 
